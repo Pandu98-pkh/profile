7414fb9a090bf7b6f2f7bd774648323cbd33d8ba
PANDU KAYA HAKIKI <61933580+Pandu98-pkh@users.noreply.github.com>
2025-01-06 22:48:08 +0700

    Update certifications.ts

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -4,7 +4,7 @@ export const certifications = [
     issuer: "Dicoding Indonesia",
     issued: "Des 2024",
     expires: "Des 2027",
-    credentialId: "6RPNY2ENRZ2M"
+    credentialId: "6RPNY2ENRZ2MID"
   },
   {
     title: "Belajar Membuat Front-End Web untuk Pemula",
@@ -12,6 +12,62 @@ export const certifications = [
     issued: "Nov 2023",
     expires: "Nov 2026",
     credentialId: "JLX1WQWOJP72"
-  }
+  },
+  {
+    title: "Belajar Dasar Manajemen Proyek",
+    issuer: "Dicoding Indonesia",
+    issued: "Okt 2023",
+    expires: "Okt 2026",
+    credentialId: "0LZ0QJ26QZ65"
+  },
+  {
+    title: "Belajar Dasar Structured Query Language (SQL)",
+    issuer: "Dicoding Indonesia",
+    issued: "Okt 2023",
+    expires: "Okt 2026",
+    credentialId: "NVP78W6GRXR0"
+  },
+  {
+    title: "Memulai Dasar Pemrograman untuk Menjadi Pengembang Software",
+    issuer: "Dicoding Indonesia",
+    issued: "Okt 2023",
+    expires: "Okt 2026",
+    credentialId: "MRZMLVENKXYQ"
+  },
+  {
+    title: "Belajar Dasar-Dasar DevOps",
+    issuer: "Dicoding Indonesia",
+    issued: "Jul 2023",
+    expires: "Jul 2026",
+    credentialId: "1OP80VML1XQK"
+  },
+  {
+    title: "Memulai Pemrograman dengan Kotlin",
+    issuer: "Dicoding Indonesia",
+    issued: "Jul 2023",
+    expires: "Jul 2026",
+    credentialId: "0LZ09N6JNZ65"
+  },
+  {
+    title: "Belajar Dasar Pemrograman JavaScript",
+    issuer: "Dicoding Indonesia",
+    issued: "Jan 2023",
+    expires: "Jan 2026",
+    credentialId: "EYX49G65OPDL"
+  },
+  {
+    title: "Cloud Practitioner Essentials (Belajar Dasar AWS Cloud)",
+    issuer: "Dicoding Indonesia",
+    issued: "Jan 2023",
+    expires: "Jan 2026",
+    credentialId: "NVP795O2GZR0"
+  },
+  {
+    title: "Memulai Pemrograman Dengan C",
+    issuer: "Dicoding Indonesia",
+    issued: "Nov 2022",
+    expires: "Nov 2025",
+    credentialId: "JLX1LW5K6X72"
+  }  
   // Add other certifications similarly
 ];
