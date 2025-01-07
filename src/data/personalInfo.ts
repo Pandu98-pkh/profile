b3461559cfb5e991519d9bb39e1ca4ada856c49d
Pandu98-pkh <pandukayahakiki98@gmail.com>
2025-01-08 05:08:47 +0700

    UPDATE

--- src/data/personalInfo.ts
+++ src/data/personalInfo.ts
@@ -4,5 +4,5 @@ export const personalInfo = {
   phone: "+6285256632471",
   linkedin: "https://linkedin.com/in/pandukayahakiki",
   github: "https://github.com/Pandu98-pkh",
-  profile: `Mahasiswa S1 Teknik Komputer dengan latar belakang organisasi di bidang teknologi informasi dan komunikasi. Berpengalaman dalam mengelola jadwal, konsep kegiatan, dan membangun hubungan antar mahasiswa. Dengan keahlian di event management dan komunikasi strategis, siap berkontribusi.`
+  profile: `Mahasiswa S1 Teknik Komputer dengan latar belakang kuat dalam organisasi di bidang teknologi informasi dan komunikasi. Memiliki pengalaman dalam memimpin inisiatif berbasis teknologi, mengembangkan strategi kegiatan, dan mendukung literasi digital. Berfokus pada pengembangan web dan event management, siap berkontribusi.`
 };
