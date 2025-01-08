90394568b74034903b5fa27133de480ba4d2d1ef
Pandu98-pkh <pandukayahakiki98@gmail.com>
2025-01-08 14:28:07 +0700

    UPDATE

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,20 +2,14 @@ import { images } from '../data/images';
 export const projects = [
   {
     title: 'Website Event ITS',
-    description: 'Microsoft Licensing webinar for Telkom University students, featuring benefits and implementation strategies.',
+    description: 'Proyek akhir Bootcamp Advanced SCHEMATICS 2024 ITS ini adalah pengembangan Website Event ITS menggunakan React. Website ini dirancang untuk memberikan informasi lengkap mengenai acara yang diselenggarakan ITS, termasuk fitur jadwal acara dan pendaftaran. Proyek ini menunjukkan kemampuan saya dalam menggunakan React untuk membuat antarmuka pengguna yang interaktif dan responsif pada tahun 2024.',
     imageUrl: images.eventITS,
     projectUrl: 'https://fp-bst-aw.vercel.app/'
   },
   {
     title: 'Form Registrasi RS',
-    description: 'Led streaming operations for various school events using OBS and vMix at SMA Negeri 1 Bangil.',
+    description: 'Proyek akhir Bootcamp Basic SCHEMATICS 2024 ITS ini merupakan pengembangan Form Registrasi Rumah Sakit menggunakan HTML, CSS, dan JavaScript. Proyek ini mencakup pembuatan formulir interaktif untuk input data pasien, validasi form, dan desain antarmuka yang responsif. Proyek ini diselesaikan pada tahun 2024 sebagai bukti pemahaman dasar saya dalam pengembangan front-end web.',
     imageUrl: images.formRS,
     projectUrl: 'https://fp-bst-bw.vercel.app/home.html'
   },
-  {
-    title: 'Ecoprint Training',
-    description: 'Organized and managed Ecoprint Training 2021 in collaboration with Yayasan Pengembangan Batik Reksa Natura Indonesia.',
-    imageUrl: 'https://img.freepik.com/premium-photo/colorful-background-with-yellow-blue-geometric-pattern_481527-25413.jpg?semt=ais_hybrid',
-    projectUrl: '#'
-  }
 ];
