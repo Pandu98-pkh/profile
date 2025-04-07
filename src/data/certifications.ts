ecac6b7aaeb9726659558b9e2fbb69a9147cb7a3
Pandu98-pkh <pandukayahakiki98@gmail.com>
2025-04-08 04:40:08 +0700

    UPDATE

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -1,3 +1,5 @@
+import { file } from "./file";
+
 export const certifications = [
   {
     title: "Belajar Dasar AI",
@@ -5,7 +7,7 @@ export const certifications = [
     issued: "Des 2024",
     expires: "Des 2027",
     credentialId: "6RPNY2ENRZ2MID",
-    imageUrl: "../src/certifications/sertifikat_course_653_742877_121224084836.pdf",
+    imageUrl: file.sc653,
   },
   {
     title: "Belajar Membuat Front-End Web untuk Pemula",
---
+++ src/data/file.ts
@@ -0,0 +1,5 @@
+import sc653 from '../certifications/sertifikat_course_653_742877_121224084836.pdf';
+
+export const file = {
+    sc653
+  };
