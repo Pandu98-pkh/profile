dd8e5728f7d27158934dd263b28be42822384f56
Pandu98-pkh <pandukayahakiki98@gmail.com>
2025-04-11 14:23:45 +0700

    UPDATE

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,7 +1,7 @@
 import avatar from '../img/DSC_0834ii71x1.jpg';
 import formRS from '../img/Cuplikan layar_7-1-2025_85327_fp-bst-bw.vercel.app.jpeg';
 import eventITS from '../img/Cuplikan layar_7-1-2025_19390_fp-bst-aw.vercel.app.jpeg';
-import bestBSTBasic from '../img/PANDU KAYA HAKIKI (5)_page-0001.jpg';
+import bestBSTBasic from '../img/Best BST Basic.jpg';
 
 export const images = {
   avatar,
